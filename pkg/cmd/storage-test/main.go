18206a295d65d7e1c936bf1b0433bd508162e71d
Andrei Matei <andrei@cockroachlabs.com>
2017-06-11 18:06:51 -0400

    chunked

--- pkg/cmd/storage-test/main.go
+++ pkg/cmd/storage-test/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"encoding/binary"
 	"flag"
 	"fmt"
 	"io"
 	"math/rand"
 	"os"
+	"os/exec"
 	"strconv"
+	"sync"
+
+	"golang.org/x/net/context"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/elastic/gosigar"
 	"github.com/pkg/errors"
 )
@@ -25,6 +29,9 @@ func check(e error) {
 }
 
 func main() {
+	chunked := flag.Bool("chunked", false, "do chunked sort")
+	tmpPath := flag.String("tmp_path", "/tmp/bench", "")
+
 	useRocksDB := flag.Bool("rocksdb", false, "use rocksdb")
 	rocksdbPath := flag.String("rocksdb_path", "/tmp/bench", "")
 	readFile := flag.String("read_file", "", "read a file")
@@ -32,7 +39,9 @@ func main() {
 	rowSizePtr := flag.Int("row_size", 200, "approximate size of each row")
 	flag.Parse()
 
-	if *useRocksDB {
+	if *chunked {
+		streamSort(*numRowsPtr, *rowSizePtr, *tmpPath)
+	} else if *useRocksDB {
 		check(writeToRocksDB(*numRowsPtr, *rowSizePtr, *rocksdbPath))
 	} else if *readFile == "" {
 		genFile(*numRowsPtr, *rowSizePtr)
@@ -170,4 +179,75 @@ func writeToRocksDB(numRows int, rowSize int, rocksdbPath string) error {
 	log.Infof(context.TODO(), "rocksdb got rows: %d", rows)
 	fmt.Printf("rocksdb got rows: %d\n", rows)
 	return nil
+}
+
+func streamSort(numRows int, rowSize int, tmpPath string) {
+	log.Infof(context.TODO(), "starting writing chunks.")
+	rowData := randBytes(rowSize)
+	rowData[len(rowData)-1] = '\n'
+
+	sortLimitMB := 370 / maxConcurrency
+	curChunkFile, curChunkPath := makeChunkFile(tmpPath, chunkNum)
+	w := bufio.NewWriter(curChunkFile)
+	for i := 0; i < numRows; i++ {
+		key := rand.Int63()
+		w.WriteString(strconv.Itoa(int(key)))
+		w.WriteRune(' ')
+		_, err := w.Write(rowData)
+		check(err)
+
+		if i != 0 && i%1000000 == 0 {
+			w.Flush()
+			log.Infof(context.TODO(), "starting sort of chunk: %d", chunkNum)
+			startChunkSort(chunkNum, curChunkPath, tmpPath, sortLimitMB)
+			chunkNum++
+			curChunkFile, curChunkPath = makeChunkFile(tmpPath, chunkNum)
+			w = bufio.NewWriter(curChunkFile)
+		}
+	}
+	w.Flush()
+	log.Infof(context.TODO(), "starting sort of final chunk: %d", chunkNum)
+	startChunkSort(chunkNum, curChunkPath, tmpPath, sortLimitMB)
+	log.Infof(context.TODO(), "finished writing chunks. Waiting for all sorts to finish.")
+	chunkWait.Wait()
+}
+
+const maxConcurrency = 5
+
+var chunkNum = 0
+var chunkWait sync.WaitGroup
+var sem = make(chan struct{}, maxConcurrency)
+var stopper = stop.NewStopper()
+
+func makeChunkFileName(tmpPath string, chunkNum int) string {
+	return fmt.Sprintf("%s/chunk-%d", tmpPath, chunkNum)
+}
+
+func makeChunkFile(tmpPath string, chunkNum int) (*os.File, string) {
+	path := makeChunkFileName(tmpPath, chunkNum)
+	f, err := os.Create(path)
+	check(err)
+	return f, path
+}
+
+func startChunkSort(chunkNum int, chunkPath string, tmpPath string, memLimitMB int) {
+	chunkWait.Add(1)
+
+	if err := stopper.RunLimitedAsyncTask(context.TODO(), sem, true, /* wait */
+		func(context.Context) {
+			cmd := exec.Command("sort",
+				"-S", fmt.Sprintf("%dM", memLimitMB),
+				"-T", tmpPath,
+				"--parallel=1",
+				"-k1.1,1.15",
+				"-s",
+				fmt.Sprintf("--output=%s/sorted-chunk-%s", tmpPath, chunkNum),
+				chunkPath)
+			check(cmd.Start())
+			check(cmd.Wait())
+			chunkWait.Done()
+			log.Infof(context.TODO(), "finished sorting of chunk: %d", chunkNum)
+		}); err != nil {
+		check(err)
+	}
 }
